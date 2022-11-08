ea1d4ba73922d22698b08757f21892bb73f7c1d5
Yuns <hope4_u@naver.com>
2022-11-08 13:13:15 +0900

    Edit the permission of S3 and Change the path of files.

--- src/middleware.js
+++ src/middleware.js
@@ -11,7 +11,8 @@ const s3 = new aws.S3({
 
 const multerUploader = multerS3({
     s3: s3,
-    bucket: 'gaemi-wetube'
+    bucket: 'gaemi-wetube',
+	acl : "pulic-read"
 })
 
 export const localsMiddleware = (req, res, next) => {
